76549fe9455667bc85811378a3968aa100b1e719
DmitryM <mikhaltchenkov@gmail.com>
2022-08-24 07:54:18 +0300

    added edit activity

--- app/src/main/java/ru/netology/nmedia/data/entities/Post.kt
+++ app/src/main/java/ru/netology/nmedia/data/entities/Post.kt
@@ -1,5 +1,9 @@
 package ru.netology.nmedia.data.entities
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Post(
     val id: Long,
     val author: String,
@@ -12,7 +16,7 @@ data class Post(
     val commentsCount: Int,
     val repostsCount: Int,
     val viewsCount: Int
-) {
+) : Parcelable {
     companion object {
         fun getEmpty() = Post(
             0,
--- app/src/main/java/ru/netology/nmedia/ui/main/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/ui/main/MainActivity.kt
@@ -1,16 +1,25 @@
 package ru.netology.nmedia.ui.main
 
 import android.os.Bundle
-import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.addTextChangedListener
+import ru.netology.nmedia.R
+import ru.netology.nmedia.data.entities.Post
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.ui.edit.EditPostActivity
 import ru.netology.nmedia.utils.NumbersFormatter
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by viewModels()
+
+    private val postContract = registerForActivityResult(EditPostActivity.Contract) { post ->
+        if (post != null) {
+            viewModel.save(post)
+            Toast.makeText(this, getString(R.string.post_saved), Toast.LENGTH_LONG).show()
+        }
+    }
 
     private lateinit var binding: ActivityMainBinding
     private val numbersFormatter = NumbersFormatter()
@@ -26,7 +35,8 @@ class MainActivity : AppCompatActivity() {
             viewModel.remove(id)
         },
         onEditListener = { post ->
-            viewModel.edit(post)
+            postContract.launch(post)
+//            viewModel.edit(post)
         }
     )
 
@@ -36,26 +46,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         with(binding) {
             recyclerView.swapAdapter(adapter, false)
-            editText.addTextChangedListener { editable -> viewModel.updateText(editable.toString()) }
-            buttonSavePost.setOnClickListener { viewModel.save() }
-            buttonCancel.setOnClickListener { viewModel.cancelEdit() }
+//            editText.addTextChangedListener { editable -> viewModel.updateText(editable.toString()) }
+//            buttonSavePost.setOnClickListener { viewModel.save() }
+//            buttonCancel.setOnClickListener { viewModel.cancelEdit() }
+            addButton.setOnClickListener { postContract.launch(Post.getEmpty()) }
         }
         viewModel.data.observe(this) { posts ->
             adapter.submitList(posts)
         }
-        viewModel.sourceMessage.observe(this) { message ->
-            with(binding) {
-                groupEdit.visibility = if (message.isNullOrEmpty()) View.GONE else View.VISIBLE
-                textEditMessage.text = message
-            }
-        }
-        viewModel.post.observe(this) { post ->
-            with(binding) {
-                if (editText.text.toString() != post?.text) {
-                    editText.setText(post?.text)
-                }
-            }
-        }
+//        viewModel.sourceMessage.observe(this) { message ->
+//            with(binding) {
+//                groupEdit.visibility = if (message.isNullOrEmpty()) View.GONE else View.VISIBLE
+//                textEditMessage.text = message
+//            }
+//        }
+//        viewModel.post.observe(this) { post ->
+//            with(binding) {
+//                if (editText.text.toString() != post?.text) {
+//                    editText.setText(post?.text)
+//                }
+//            }
+//        }
 
     }
 
